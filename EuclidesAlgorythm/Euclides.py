dbd9ad2b55d8316bc64444ad1165eb9a10e968ac
Adam <73337380+Rogowiak@users.noreply.github.com>
2021-11-02 22:56:47 +0100

    Update Euclides.py

--- EuclidesAlgorythm/Euclides.py
+++ EuclidesAlgorythm/Euclides.py
@@ -14,10 +14,17 @@ def EuclidesDivision(a,b):
     if a == b:
         print(a)
         return a
+    if a == 0:
+        print(b)
+        return b
+    if b == 0:
+        print(a)
+        return a 
     else:
         if a > b:
             a = a % b
         elif a < b:
             b = b % a
-    return EuclidesSubstraction(a,b)
+    return EuclidesDivision(a,b)
+
 
